669b67a1840ff79a1ad3f594a438e32885c3abd5
Jared Morgan <jmorgan@morgancreative.net>
2008-07-11 22:08:45 +0000

    Comment - update properties with auto-validation triggers.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,10 @@ class Comment
   include DataMapper::Resource
 
     property :id, Integer, :serial => true
-    property :author, String
-    property :email, String
-    property :body, Text
-    property :article_id, Integer
+    property :author, String, :nullable => false, :length => 100
+    property :email, String, :format => :email_address
+    property :body, Text, :nullable => false
+    property :article_id, Integer, :nullable => false
     property :parent_id, Integer
     property :created_at, DateTime
     property :updated_at, DateTime
