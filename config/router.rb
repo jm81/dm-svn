1fa0b32de9808e7541ff5ebf292bedf9bcbea667
Jared Morgan <jmorgan@morgancreative.net>
2008-07-11 21:54:03 +0000

    Fix typo in router (had used 'posts' instead of 'comments'. oops)

--- config/router.rb
+++ config/router.rb
@@ -23,7 +23,7 @@ Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do |r|
   # RESTful routes
   r.resources :articles
-  r.resources :posts
+  r.resources :comments
 
   # This is the default route for /:controller/:action/:id
   # This is fine for most cases.  If you're heavily using resource-based
