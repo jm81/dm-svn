fcf0167bfa9b59236db6fb54aa5da882704e3409
Jared Morgan <jmorgan@morgancreative.net>
2008-07-12 00:48:44 +0000

    Add some specs for Comment (validations)

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,6 +2,34 @@ require File.join( File.dirname(__FILE__), "..", "spec_helper" )
 
 describe Comment do
 
-  it "should have specs"
+  before(:each) do
+    @comment = Comment.new
+    @comment.author = "Jane Doe"
+    @comment.body = "This post was foolish"
+    @comment.article_id = 1
+  end
+  
+  it "should be valid" do
+    @comment.should be_valid
+  end
+
+  it "should require an author" do
+    @comment.author = ''
+    @comment.should_not be_valid
+  end
+  
+  it "should require a body" do
+    @comment.body = ''
+    @comment.should_not be_valid
+  end
+  
+  it "should validate email format" do
+    @comment.email = nil
+    @comment.should be_valid # Allow nil
+    @comment.email = 'jane'
+    @comment.should_not be_valid
+    @comment.email = 'jane@example.com'
+    @comment.should be_valid
+  end
 
 end
