bb641b29ba87c9fb651f584bb9ccf7e77050d6e6
Jared Morgan <jmorgan@morgancreative.net>
2008-07-16 01:20:07 +0000

    Add 'merb-haml' dependency in init.rb

--- config/init.rb
+++ config/init.rb
@@ -57,7 +57,7 @@ Gem.path.unshift(Merb.root / "gems")
 # dependency "RedCloth", "> 3.0"
 # OR
 # dependencies "RedCloth" => "> 3.0", "ruby-aes-cext" => "= 1.0"
-dependencies "merb_helpers", "merb-assets"
+dependencies "merb_helpers", "merb-assets", 'merb-haml'
 dependency 'dm-validations', '= 0.9.1'
 dependency 'dm-timestamps', '= 0.9.1'
 dependency 'dm-aggregates', '= 0.9.1'
