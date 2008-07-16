9d4b4b648b65ee413815c3da8419535df4b57f0b
Jared Morgan <jmorgan@morgancreative.net>
2008-07-16 01:20:59 +0000

    Update routes to nest comments in articles and add default route (Articles#index).

--- config/router.rb
+++ config/router.rb
@@ -21,16 +21,9 @@
 
 Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do |r|
-  # RESTful routes
-  r.resources :articles
-  r.resources :comments
-
-  # This is the default route for /:controller/:action/:id
-  # This is fine for most cases.  If you're heavily using resource-based
-  # routes, you may want to comment/remove this line to prevent
-  # clients from calling your create or destroy actions with a GET
-  r.default_routes
+  r.resources :articles do | article |
+    article.resources :comments
+  end
   
-  # Change this for your home page to be available at /
-  # r.match('/').to(:controller => 'whatever', :action =>'index')
+  r.match('/').to(:controller => 'articles', :action =>'index')
 end
