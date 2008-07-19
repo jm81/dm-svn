cbc957880895f76398d423bfe9a0369a2f93e702
Jared Morgan <jmorgan@morgancreative.net>
2008-07-19 19:50:03 +0000

    Update Wistle::Config to add additional settings (uri, username, password, property_prefix, extension), and accept settings from database.yml

--- lib/wistle/config.rb
+++ lib/wistle/config.rb
@@ -1,10 +1,37 @@
 module Wistle
   class Config
-    attr_accessor :body_property
+    OPTS = [:uri, :username, :password,
+            :body_property, :property_prefix, :extension]
+    
+    attr_accessor *OPTS
     
     def initialize
       # Set defaults
       @body_property = 'body'
+      @property_prefix = 'ws:'
+      @extension = 'txt'
+
+      # Try to set variables from database.yml.
+      # The location of database.yml should, I suppose, be configurable.
+      # Oh, well.
+      if Object.const_defined?("RAILS_ROOT")
+        f = "#{RAILS_ROOT}/config/database.yml"
+        env = Kernel.const_defined?("RAILS_ENV") ? RAILS_ENV : "development"
+      elsif Object.const_defined?("Merb")
+        f = "#{Merb.root}/config/database.yml"
+        env = Merb.env.to_sym || :development
+      end
+      
+      if f
+        config = YAML.load(IO.read(f))[env]
+        OPTS.each do |field|
+          config_field = config["svn_#{field}"] || config["svn_#{field}".to_sym]
+          if config_field
+            instance_variable_set("@#{field}", config_field)
+          end
+        end
+      end
+      
     end
   end
 end
--- spec/wistle/config_spec.rb
+++ spec/wistle/config_spec.rb
@@ -14,4 +14,25 @@ describe Wistle::Config do
     @c.body_property = 'contents'
     @c.body_property.should == 'contents'
   end
+  
+  it "should initialize @property_prefix to 'ws:'" do
+    @c.property_prefix.should == 'ws:'
+  end
+  
+  it "should initialize @extension to 'txt'" do
+    @c.extension.should == 'txt'
+  end
+  
+  it "should load config options from database.yml" do
+    f = "#{Merb.root}/config/database.yml"
+    puts f
+    yaml = File.read(File.dirname(__FILE__) + '/database.yml')
+    IO.should_receive(:read).with(f).and_return(yaml)
+ 
+    c = Wistle::Config.new
+    c.username.should == 'login'
+    c.password.should == 'pw1234'
+    c.extension.should == 'doc'
+    c.body_property.should == 'body' # Didn't change
+  end
 end
