44331af66b49484fb7e44c480d7a38fb30523c1c
Jared Morgan <jmorgan@morgancreative.net>
2008-07-19 19:44:05 +0000

    Update Comment#updated_at test to sleep for 1/10 of a second to avoid occassional "too-quick" updates.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -66,6 +66,7 @@ describe Comment do
     @comment.save
     u = @comment.updated_at
     u.should_not be_nil
+    sleep(0.1)
     @comment.body = "New Body"
     @comment.save
     @comment.updated_at.should > u
