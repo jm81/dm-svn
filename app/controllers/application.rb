e16c3d1c57292a0d97a7668c4e44c8dc509dd4ca
Jared Morgan <jmorgan@morgancreative.net>
2008-07-25 20:52:32 +0000

    Update Application and Articles controller to filter (MOL) by site.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,9 +1,14 @@
 class Application < Merb::Controller
   before :sync_articles
+  before :choose_site
   
   protected
 
   def sync_articles
-    Article.sync
+    Site.sync_all
+  end
+  
+  def choose_site
+    @site = Site.by_domain(request.host)
   end
 end
--- app/controllers/articles.rb
+++ app/controllers/articles.rb
@@ -2,12 +2,12 @@ class Articles < Application
   # provides :xml, :yaml, :js
 
   def index
-    @articles = Article.published
+    @articles = Article.published(:site_id => @site.id)
     display @articles
   end
 
   def show
-    @article = Article.get(params[:id])
+    @article = Article.first(:id => params[:id], :site_id => @site.id)
     raise NotFound unless @article
     display @article
   end
