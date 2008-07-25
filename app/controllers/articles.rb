7f001dfa5371a72226fd6be2812c58fd24368419
Jared Morgan <jmorgan@morgancreative.net>
2008-07-25 20:45:39 +0000

    Remove new, create, edit, update and destroy actions from Articles controller (no longer needed).

--- app/controllers/articles.rb
+++ app/controllers/articles.rb
@@ -11,47 +11,5 @@ class Articles < Application
     raise NotFound unless @article
     display @article
   end
-
-  def new
-    only_provides :html
-    @article = Article.new
-    render
-  end
-
-  def edit
-    only_provides :html
-    @article = Article.get(params[:id])
-    raise NotFound unless @article
-    render
-  end
-
-  def create
-    @article = Article.new(params[:article])
-    if @article.save
-      redirect url(:article, @article)
-    else
-      render :new
-    end
-  end
-
-  def update
-    @article = Article.get(params[:id])
-    raise NotFound unless @article
-    if @article.update_attributes(params[:article]) || !@article.dirty?
-      redirect url(:article, @article)
-    else
-      raise BadRequest
-    end
-  end
-
-  def destroy
-    @article = Article.get(params[:id])
-    raise NotFound unless @article
-    if @article.destroy
-      redirect url(:article)
-    else
-      raise BadRequest
-    end
-  end
-
+  
 end
