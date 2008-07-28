134d1995fdfebb33a558015c26ab70e580e2c5fe
Jared Morgan <jmorgan@morgancreative.net>
2008-07-28 02:03:35 +0000

    Update #asset_path and #image_tag in GlobalHelpers to use site-specific public paths.

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -24,5 +24,19 @@ module Merb
     def article_url(article)
       "/#{article.path}"
     end
+    
+    def image_tag(img, opts ={})
+      opts[:path] ||= "/sites/#{@site.name}/images/"
+      super(img, opts)
+    end
+    
+    def asset_path(asset_type, filename, local_path = false)
+      path = super(asset_type, filename, local_path)
+      "/sites/#{@site.name}#{path}"
+    end
+    
+    def ads(path)
+      partial("ads/#{path}") if Merb::environment == "production"
+    end
   end
 end
