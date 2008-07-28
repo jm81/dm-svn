4d76b74561946b07b29603e9bb6478ee0dd317cf
Jared Morgan <jmorgan@morgancreative.net>
2008-07-28 02:02:53 +0000

    Override Application#render to temporarily add Site's view path to template_roots to allow site-specific views.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,4 +1,11 @@
-class Application < Merb::Controller
+class Application < Merb::Controller 
+  def render(thing = nil, opts = {})
+    self.class._template_roots << ["/u/apps/w2/app/sites/#{@site.name}/views", :_template_location]
+    ret = super
+    self.class._template_roots.pop
+    ret
+  end
+  
   before :sync_articles
   before :choose_site
   
@@ -11,4 +18,5 @@ class Application < Merb::Controller
   def choose_site
     @site = Site.by_domain(request.host)
   end
+
 end
