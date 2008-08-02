ea67321a68fe9bc2e87e765025e702eb323b0c25
Jared Morgan <jmorgan@morgancreative.net>
2008-08-02 03:56:34 +0000

    Fix Application#render to use Merb.root instead of hard-coded root folder.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,6 +1,6 @@
 class Application < Merb::Controller 
   def render(thing = nil, opts = {})
-    self.class._template_roots << ["/u/apps/w2/app/sites/#{@site.name}/views", :_template_location]
+    self.class._template_roots << ["#{Merb.root}/app/sites/#{@site.name}/views", :_template_location]
     ret = super
     self.class._template_roots.pop
     ret
