0bcd51cd75bf1c152160c67d381e97ee026a3fc9
Jared Morgan <jmorgan@morgancreative.net>
2008-08-02 03:30:15 +0000

    Updates to mephisto migration script. Still not for general use.

--- lib/migrations/mephisto.rb
+++ lib/migrations/mephisto.rb
@@ -16,7 +16,11 @@ class Content
     property :author, String
     property :author_email, String
     property :site_id, Integer
+    property :version, Integer
     property :filter, String
+    property :created_at, DateTime
+    property :updated_at, DateTime
+    property :published_at, DateTime
   end
 end
 
@@ -34,7 +38,9 @@ class ContentVersion
     property :author_email, String
     property :site_id, Integer
     property :filter, String
+    property :created_at, DateTime
     property :updated_at, DateTime
+    property :published_at, DateTime
     property :content_id, Integer
     property :version, Integer
     property :versioned_type, String
@@ -62,8 +68,33 @@ class Section
   end
 end
 
+class MephistoTagging
+  include DataMapper::Resource
+
+  repository(:mephisto) do
+    @storage_names[:mephisto] = 'taggings'
+    
+    property :id, Integer, :serial => true
+    property :taggable_id, Integer
+    property :taggable_type, String
+    property :tag_id, Integer
+  end
+end
+
+class MephistoTag
+  include DataMapper::Resource
+  
+  repository(:mephisto) do
+    @storage_names[:mephisto] = 'tags'
+    
+    property :id, Integer, :serial => true
+    property :name, String
+  end
+end
+
 # Check to run for multiple permalinks:
 # select permalink, count(id) as c from contents where type='Article' group by permalink having c > 1;
+# select article_id, count(article_id) as c from assigned_sections group by article_id having c > 1;
 
 module Migrations
   class Mephisto
@@ -77,10 +108,20 @@ module Migrations
 
       svn_repo(@name) do
         rev = 1
-        revision(rev, 'Generate content directory (Wistle Migration)',
+        revision(rev, 'Generate tags, trunk and branches directories (Wistle Migration)',
             :author => 'jmorgan',
-            :date => Time.parse("2007-01-01")) do
-          dir 'content'
+            :date => Time.parse("2005-10-01")) do
+          dir 'tags'
+          dir 'branches'
+          dir 'trunk'
+        end
+        
+        revision(rev += 1, 'Generate articles directory (Wistle Migration)',
+            :author => 'jmorgan',
+            :date => Time.parse("2005-10-01")) do
+          dir 'trunk' do
+            dir 'articles'
+          end
         end
 
         repository(:mephisto) do
@@ -88,37 +129,106 @@ module Migrations
 
           versions.each do |v|
             article = Content.get(v.article_id)
-            section_id = AssignedSection.first(:article_id => v.article_id).section_id
-            section = Section.get(section_id).path
+            
+            section_paths = []
+            AssignedSection.all(:article_id => v.article_id).each do |as|
+              section_path = Section.get(as.section_id).path
+              section_paths << section_path unless section_path.blank?
+            end
+            
+            if section_paths.length != 1
+              puts "Sections error: #{article.permalink}:#{article.id} - #{section_paths.length} sections: #{section_paths.join(' ;')}"
+            end
+            section = section_paths[0]
+            
             commit_type = v.version == 1 ? "Create" : "Update"
+            tags = []
+            MephistoTagging.all(:taggable_id => v.article_id.to_i, :taggable_type => 'Content').each do |tagging|
+              # repository(:mephisto) { MephistoTagging.all }
+              if site_id == 2
+                tags << MephistoTag.get(tagging.tag_id).name.downcase
+              else
+                tags << MephistoTag.get(tagging.tag_id).name
+              end
+            end
+            tags = tags.join('; ')
             
             revision(rev += 1,
-                "#{commit_type} content: #{section}/#{article.permalink} (Wistle Migration)",
+                "#{commit_type} article: #{section}/#{article.permalink} (Wistle Migration)",
                 :author => 'jmorgan', #TODO: real name
                 :date => v.updated_at
               ) do
   
-              dir 'content' do
-                dir section do
-                  file "#{article.permalink}.txt" do
-                    prop 'ws:title', v.title
-                    filter = v.filter.gsub('_filter', '').camel_case
-                    filter = "Smartypants; Markdown" if filter == "Smartypants"
-                    prop 'ws:filter', filter
-                    body v.body
+              dir 'trunk' do
+                dir 'articles' do
+                  dir section do
+                    file "#{article.permalink}.txt" do
+                      # Ensure we are using the latest data.
+                      v = article if v.version == article.version
+                      
+                      prop 'ws:title', v.title
+                      filter = v.filter.gsub('_filter', '').camel_case
+                      filter = "Smartypants; Markdown" if filter == "Smartypants"
+                      filter = "Smartypants; Markdown" if filter == "Blank"
+                      filter = "BibleML" if filter == "Bible"
+                      prop('ws:filters', filter) unless filter.blank?
+                      prop('ws:tags', tags) unless tags.blank?
+                      if v.published_at
+                        prop 'ws:published_at', v.published_at.strftime("%Y-%m-%d %H:%M:%S")
+                      end
+                      body v.body
+                    end
+                    
                   end
-                end
-              end # dir
+                end # dir
+              end #trunk
             end # revision
             
           end
         end
       end
       
-      svn_repo(@name).checkout.commit
+      svn_repo(@name).create.commit
       return true
+    end
+    
+    def comments
+      site_id = @site_id
+      
+      repository(:mephisto) do
+        comments = Content.all(:site_id => site_id, :type => 'Comment', :order => [:created_at.asc])
+        ary = []
+        
+        comments.each do |c|
+          ary << {
+            :author => c.author,
+            :email => c.author_email,
+            :body => c.body,
+            :created_at => c.created_at,
+            :updated_at => c.updated_at,
+            :published_at => c.published_at,
+            :article => Content.get(c.article_id).permalink
+          }
+        end
+        File.open("#{site_id}.yml", 'w') {|f| f.write(ary.to_yaml) }
+      end
+    end
+    
+    def fix_site_ids
+      repository(:mephisto) do 
+        Content.all( :type => 'Article').each do |content|
+          ContentVersion.all(:article_id => content.id).each do |cv|
+            if cv.site_id != content.site_id
+              puts "#{content.id}: #{cv.id} (#{content.site_id}) - #{content.permalink}"
+              cv.update_attributes(:site_id => content.site_id)
+            end
+          end
+        end
+      end
+      true
     end
   end
 end
 
-# m = Migrations::Mephisto.new("repo_name", 1)
+# load 'lib/migrations/mephisto.rb'
+# m = Migrations::Mephisto.new("repo1", 1)
