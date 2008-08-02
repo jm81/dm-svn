a795117380a4f3f6a95eb3a9290b4c18a8d9b4d3
Jared Morgan <jmorgan@morgancreative.net>
2008-08-02 17:39:24 +0000

    Attempt to get site-view rendering working in production environment.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,17 +1,19 @@
-class Application < Merb::Controller 
-  def render(thing = nil, opts = {})
-    self.class._template_roots << ["#{Merb.root}/app/sites/#{@site.name}/views", :_template_location]
-    ret = super
-    self.class._template_roots.pop
-    ret
-  end
-  
+class Application < Merb::Controller  
   before :choose_site
+  before :update_template_roots
+  after :revert_template_roots
   
   protected
   
   def choose_site
     @site = Site.by_domain(request.host)
   end
+  
+  def update_template_roots
+    self.class._template_roots << ["#{Merb.root}/app/sites/#{@site.name}/views", :_template_location]
+  end
 
+  def revert_template_roots
+    self.class._template_roots.pop
+  end
 end
