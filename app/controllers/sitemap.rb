c017ee909f91e212d58a17a496dcff4c5ddacf16
Jared Morgan <jmorgan@morgancreative.net>
2008-08-19 19:29:47 +0000

    Sitemap#index: better method of getting time of last article update.

--- app/controllers/sitemap.rb
+++ app/controllers/sitemap.rb
@@ -5,7 +5,7 @@ class Sitemap < Application
     @articles = @site.published_by_category
     @categories = @site.categories
     @tags = @site.tags
-    @site_updated_at = @site.articles.first(:order => [:updated_at.desc]).updated_at
+    @site_updated_at = @site.articles.min(:updated_at)
     @domain = "http://#{request.host}/"
 
     headers["Last-Modified"] = Time.parse(@site_updated_at.to_s).httpdate
