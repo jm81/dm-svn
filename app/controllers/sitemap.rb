32c7e31558644b35ff9f4c999e5c00f77f5d0d82
Jared Morgan <jmorgan@morgancreative.net>
2008-08-19 19:30:25 +0000

    Sitemap#index: Using #max, instead of #min on last article update.

--- app/controllers/sitemap.rb
+++ app/controllers/sitemap.rb
@@ -5,7 +5,7 @@ class Sitemap < Application
     @articles = @site.published_by_category
     @categories = @site.categories
     @tags = @site.tags
-    @site_updated_at = @site.articles.min(:updated_at)
+    @site_updated_at = @site.articles.max(:updated_at)
     @domain = "http://#{request.host}/"
 
     headers["Last-Modified"] = Time.parse(@site_updated_at.to_s).httpdate
