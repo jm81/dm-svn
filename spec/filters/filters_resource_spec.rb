5cbf19c5e86438f272a40d5ec6d28dc368e9529c
Jared Morgan <jmorgan@morgancreative.net>
2008-09-12 21:13:12 +0000

    Remove require of filters lib in filters specs.
    It's not needed and generates already loaded constant warnings.

--- spec/filters/filters_resource_spec.rb
+++ spec/filters/filters_resource_spec.rb
@@ -1,5 +1,4 @@
 require File.join( File.dirname(__FILE__), "..", "spec_helper" )
-require File.join( File.dirname(__FILE__), "..", "..", "lib", "filters.rb" )
 
 class MockContent
   include DataMapper::Resource
