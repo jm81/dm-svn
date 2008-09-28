2c4ee29a1a0a55966259ff541f2e9a9c0b0c9ab9
Jared Morgan <jmorgan@morgancreative.net>
2008-09-28 02:28:43 +0000

    Update Linebreaker filter to also handle \r\n breaks.

--- lib/filters/linebreaker.rb
+++ lib/filters/linebreaker.rb
@@ -6,7 +6,9 @@ class Linebreaker
   end
   
   def to_html
-    @input.gsub(/(\S)\n(\S)/, "\\1<br />\n\\2")
+    @input.
+      gsub(/(\S)\n(\S)/, "\\1<br />\n\\2").
+      gsub(/(\S)\r\n(\S)/, "\\1<br />\n\\2")
   end
   
 end
